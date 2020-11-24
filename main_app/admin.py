1dc1d73eda67418429c87f86c512375693723baf
donavonelli <donavonelli@yahoo.com>
2020-11-24 12:48:24 -0500

    updated

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Newsletter
 
 # Register your models here.
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'email',)
+
+
+admin.site.register(Newsletter, NewsletterAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Newsletter(models.Model):
+    first_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50, unique=True)
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f" User first name: {self.first_name} Email: {self.email}"
